833efa6a4a9e6a3dd8bbe825c71fca3d68295b9e
natasha1202 <82150665+natasha1202@users.noreply.github.com>
2021-04-09 13:35:57 +0300

    Lesson 1.3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,6 +6,20 @@ public class MyFirstProgram {
         
         System.out.println("Hello World!");
 
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+
+        System.out.println(1/2);
+        System.out.println(1.0/2);
+        System.out.println(2.0/2);
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+        System.out.println(2 + 2*2);
+        System.out.println((2+2)*2);
+        System.out.println("2 + 2 = "+ (2+2));
+
     }
     
 }
