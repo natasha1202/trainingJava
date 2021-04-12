ef651d1b467329347f458ffc44388914c6414501
natasha1202 <82150665+natasha1202@users.noreply.github.com>
2021-04-12 15:48:14 +0300

    New methods added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,30 +3,52 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Hello World!");
 
-        System.out.println(2+2);
-        System.out.println(2*2);
-        System.out.println(2/2);
-        System.out.println(2-2);
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
 
-        System.out.println(1/2);
-        System.out.println(1.0/2);
-        System.out.println(2.0/2);
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println(2.0 / 2);
         System.out.println("2" + "2");
         System.out.println("2" + 2);
-        System.out.println(2 + 2*2);
-        System.out.println((2+2)*2);
-        System.out.println("2 + 2 = "+ (2+2));
+        System.out.println(2 + 2 * 2);
+        System.out.println((2 + 2) * 2);
+        System.out.println("2 + 2 = " + (2 + 2));
 
-        int l = 6;
-        int s = l*l;
-        System.out.println("Площадь квадрата со стороной " + l +  " = " + s);
+        int l2 = 6;
+        int s = l2 * l2;
+        System.out.println("Площадь квадрата со стороной " + l2 + " = " + s);
 
         String somebody = "world";
+
+        // Lesson 1.7 Methods
+
         System.out.println("Hello, " + somebody + " !");
+        hello("world");
+        hello("user");
+        hello("Alexei");
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l) );
+        double a =4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами  " + a + " и " + b  + " = " + area(a,b) );
 
     }
-    
+
+
+        public static void hello(String somebody2){
+            System.out.println("Hello, " + somebody2 + " !");
+
+    }
+        public static double area(double l){
+        return l*l;
+    }
+        public static double area(double a, double b){
+        return a*b;
+    }
 }
