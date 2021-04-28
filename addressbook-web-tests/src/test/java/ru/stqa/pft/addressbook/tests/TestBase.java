823fad4b81ff06f0e6d2b21c029bf89aa4013bfe
natasha1202 <82150665+natasha1202@users.noreply.github.com>
2021-04-28 16:20:07 +0300

    Lesson3.3 IEDriver, ChromeDriver added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.util.concurrent.TimeUnit;
+
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod
     public void setUp() throws Exception {
@@ -17,6 +20,7 @@ public class TestBase {
     public void tearDown() {
         app.logout();
         app.stop();
+        app.wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
 
 }
