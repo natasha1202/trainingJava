ccbecc3978b125e9d6ce96a85d90e26f84065028
natasha1202 <82150665+natasha1202@users.noreply.github.com>
2021-04-30 15:28:36 +0300

    Lesson3.5 null-values

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -17,8 +17,10 @@ public class HelperBase {
 
     protected void type(By locator, String text) {
         click(locator);
+        if (text != null){
         wd.findElement(locator).clear();
         wd.findElement(locator).sendKeys(text);
+        }
     }
 
     public boolean isAllertPresent() {
