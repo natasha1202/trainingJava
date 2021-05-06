540d8d54fffefdfd71b34715cb9b004fad9c89ba
natasha1202 <82150665+natasha1202@users.noreply.github.com>
2021-05-06 17:33:36 +0300

    Lesson4.5 Form collection

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -4,20 +4,26 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test01", "test", "text"));
         }
-        app.getGroupHelper().selectGroup(before-1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        // int before = app.getGroupHelper().getGroupCount();
+        //app.getGroupHelper().selectGroup(before -1);
+        app.getGroupHelper().selectGroup(before.size() -1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returntoGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before - 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        //int after = app.getGroupHelper().getGroupCount();
+        //Assert.assertEquals(after, before - 1);
+        Assert.assertEquals(after.size(), before.size() - 1);
     }
 
 }
