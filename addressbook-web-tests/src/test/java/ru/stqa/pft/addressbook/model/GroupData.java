9864938d578bbea68c8bb0f164adfc475e3b4f3c
natasha1202 <82150665+natasha1202@users.noreply.github.com>
2021-05-07 15:42:55 +0300

    Lesson4.7 set

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,8 +4,17 @@ public class GroupData {
     private final String groupName;
     private final String groupHeader;
     private final String groupFooter;
+    private final String id;
+
+    public GroupData(String id,String groupName, String groupHeader, String groupFooter) {
+        this.id = id;
+        this.groupName = groupName;
+        this.groupHeader = groupHeader;
+        this.groupFooter = groupFooter;
+    }
 
     public GroupData(String groupName, String groupHeader, String groupFooter) {
+        this.id = null;
         this.groupName = groupName;
         this.groupHeader = groupHeader;
         this.groupFooter = groupFooter;
@@ -13,6 +22,36 @@ public class GroupData {
 
     public String getGroupName() {
         return groupName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (groupName != null ? !groupName.equals(groupData.groupName) : groupData.groupName != null) return false;
+        return id != null ? id.equals(groupData.id) : groupData.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = groupName != null ? groupName.hashCode() : 0;
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "groupName='" + groupName + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getGroupHeader() {
@@ -23,25 +62,4 @@ public class GroupData {
         return groupFooter;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return groupName != null ? groupName.hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "groupName='" + groupName + '\'' +
-                '}';
-    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -18,12 +19,17 @@ public class GroupModificationTests extends TestBase {
         // app.getGroupHelper().selectGroup(before-1);
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test01", "test02", "test03"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test01", "test02", "test03");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returntoGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         //int after = app.getGroupHelper().getGroupCount();
         // Assert.assertEquals(after, before);
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before) ,new HashSet<Object>(after));
     }
 }
