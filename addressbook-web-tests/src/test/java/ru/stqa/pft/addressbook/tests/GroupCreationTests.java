a2d28093a357afd3f2917b01f2b685491e022c92
natasha1202 <82150665+natasha1202@users.noreply.github.com>
2021-08-10 20:17:07 +0300

    Lesson 6.3-6.4 jCommander, dataProvider for groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,22 +4,39 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
 
 public class GroupCreationTests extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validGroups(){
+        List<Object[]> list = new ArrayList<Object[]>();
+       // list.add(new Object[]{"test1", "header1", "footer1"});
+       // list.add(new Object[]{"test2", "header2", "footer2"});
+       // list.add(new Object[]{"test3", "header3", "footer3"});
+        list.add(new Object[] {new GroupData().withGroupName("test1").withGroupFooter("header1").withGroupHeader("footer1")});
+        list.add(new Object[] {new GroupData().withGroupName("test2").withGroupFooter("header2").withGroupHeader("footer2")});
+        list.add(new Object[] {new GroupData().withGroupName("test3").withGroupFooter("header3").withGroupHeader("footer3")});
+        return list.iterator();
+    }
 
 
-    @Test
-    public void testGroupCreation() {
+    @Test (dataProvider = "validGroups")
+    //public void testGroupCreation(String name, String header, String footer) {
+    public void testGroupCreation(GroupData group) {
+        //GroupData group = new GroupData().withGroupName(name).withGroupFooter(header).withGroupHeader(footer);
         app.goTo().groupPage();
         // List<GroupData> before = app.group().groupList();
         //Set<GroupData> before = app.group().all();
         Groups before = (Groups) app.group().all();
         // int before = app.getGroupHelper().getGroupCount();
         // GroupData group = new GroupData("test22", null, null);
-        GroupData group = new GroupData().withGroupName("test12").withGroupFooter("test").withGroupHeader("text");
+        //GroupData group = new GroupData().withGroupName("test12").withGroupFooter("test").withGroupHeader("text");
         app.group().create(group);
         // List<GroupData> after = app.group().groupList();
         //Set<GroupData> after = app.group().all();
