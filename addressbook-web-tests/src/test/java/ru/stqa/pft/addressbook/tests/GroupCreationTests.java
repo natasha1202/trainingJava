ae1979ae8dede998f722e905f6605d461bf21efe
natasha1202 <82150665+natasha1202@users.noreply.github.com>
2021-08-19 21:19:01 +0300

    HomeWork Task 15 part1 tests for groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -66,100 +66,26 @@ public class GroupCreationTests extends TestBase {
         return list.iterator();}
     }
 
-    /*
-    @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
-       List<Object[]> list = new ArrayList<Object[]>();
-       list.add(new Object[]{"test1", "header1", "footer1"});
-       list.add(new Object[]{"test2", "header2", "footer2"});
-       list.add(new Object[]{"test3", "header3", "footer3"});
-        return list.iterator();
-    }
-    */
-
-
     @Test (dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) {
+        Groups before = app.db().groups();
         app.goTo().groupPage();
-        Groups before = (Groups) app.group().all();
         app.group().create(group);
         assertThat(app.group().groupCount(), equalTo(before.size() + 1));
-        Groups after = (Groups) app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
     }
 
     @Test
     public void testBadGroupCreation() {
+        Groups before = app.db().groups();
         app.goTo().groupPage();
-        Groups before = (Groups) app.group().all();
         GroupData group = new GroupData().withGroupName("test2'");
         app.group().create(group);
         assertThat(app.group().groupCount(), equalTo(before.size()));
-        Groups after = (Groups) app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before));
     }
 
-        /*
-     @Test (dataProvider = "validGroups")
-    //public void testGroupCreation() {
-        app.goTo().groupPage();
-        // List<GroupData> before = app.group().groupList();
-        // Set<GroupData> before = app.group().all();
-        Groups before = (Groups) app.group().all();
-        // int before = app.getGroupHelper().getGroupCount();
-        // GroupData group = new GroupData("test22", null, null);
-        GroupData group = new GroupData().withGroupName("test12").withGroupFooter("test").withGroupHeader("text");
-        app.group().create(group);
-        // List<GroupData> after = app.group().groupList();
-        // Set<GroupData> after = app.group().all();
-        assertThat(app.group().groupCount(), equalTo(before.size() + 1));
-        Groups after = (Groups) app.group().all();
-        // int after = app.getGroupHelper().getGroupCount();
-        // Assert.assertEquals(after, before + 1);
-        // Assert.assertEquals(after.size(), before.size() + 1);
-        // assertThat(after.size(), equalTo(before.size() + 1));
-
-    // after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
-    // group.setId(max);
-    // group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt());
-    // before.add(group);
-    // Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    // before.sort(byId);
-    // after.sort(byId);
-    // Assert.assertEquals(new HashSet<Object>(before) ,new HashSet<Object>(after));
-    // Assert.assertEquals(before, after);
-    // assertThat(after, equalTo(before));
-    // assertThat(after, equalTo(before.withAdded(group)));
-    assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
-    }
-     */
-
-    /*
-     @Test (dataProvider = "validGroups")
-    public void testGroupCreation(String name, String header, String footer) {
-        GroupData group = new GroupData().withGroupName(name).withGroupFooter(header).withGroupHeader(footer);
-        app.goTo().groupPage();
-        Groups before = (Groups) app.group().all();
-        app.group().create(group);
-        assertThat(app.group().groupCount(), equalTo(before.size() + 1));
-        Groups after = (Groups) app.group().all();
-        assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
-    }
-     */
-
-    /*
-     @Test (dataProvider = "validGroups")
-    public void testGroupCreation(GroupData group) {
-        app.goTo().groupPage();
-        Groups before = (Groups) app.group().all();
-        app.group().create(group);
-        assertThat(app.group().groupCount(), equalTo(before.size() + 1));
-        Groups after = (Groups) app.group().all();
-        assertThat(after, equalTo(
-            before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
-    }
-     */
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,48 +1,33 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.hamcrest.CoreMatchers;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
-        app.goTo().groupPage();
-        if ( app.group().all().size() == 0){
-           // app.group().createGroup(new GroupData("test01", "test", "text"));
+        if ( app.db().groups().size() == 0){
+            app.goTo().groupPage();
             app.group().create(new GroupData().withGroupName("removeGroup"));
         }
     }
 
     @Test
     public void testGroupDeletion() {
-        //List<GroupData> before = app.group().groupList();
-        //Set<GroupData> before = app.group().all();
-        Groups before = (Groups) app.group().all();
+        Groups before = app.db().groups();
         GroupData deletedGroup = before.iterator().next();
-        int index = before.size() - 1;
-        // int before = app.getGroupHelper().getGroupCount();
-        //app.getGroupHelper().selectGroup(before -1);
-        //app.group().delete(index);
+        app.goTo().groupPage();
         app.group().delete(deletedGroup);
-        // List<GroupData> after = app.group().groupList();
-        // Set<GroupData> after = app.group().all();
         assertThat(app.group().groupCount(), equalTo(before.size() - 1));
-        Groups after = (Groups) app.group().all();
-        //int after = app.getGroupHelper().getGroupCount();
-        //Assert.assertEquals(after, before - 1);
-        //assertEquals(after.size(), before.size() - 1);
-
-        //before.remove(index);
-        //before.remove(deletedGroup);
+        Groups after = app.db().groups();
         assertThat(after, CoreMatchers.equalTo(before.without(deletedGroup)));
-        //    Assert.assertEquals(before, after);
 
     }
 
