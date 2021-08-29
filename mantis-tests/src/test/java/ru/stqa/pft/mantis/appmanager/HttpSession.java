2086cefbd879a625488bcd49410e90d3da291ceb
natasha1202 <82150665+natasha1202@users.noreply.github.com>
2021-08-29 15:49:24 +0300

    Lesson 8.3 configuration updated

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -1,18 +1,19 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.apache.http.impl.client.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpSession {
     private CloseableHttpClient httpclient;
@@ -20,15 +21,16 @@ public class HttpSession {
 
     public HttpSession(ApplicationManager app){
         this.app = app;
-        httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).buid();
-        httpclient = HttpClients
+        httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
     }
 
     public boolean login(String username, String password) throws IOException {
         HttpPost post = new HttpPost (app.getProperty("web.baseUrl")+"/login.php");
+       // HttpPost postLogin = new HttpPost (app.getProperty("web.baseUrl")+"/login_page.php");
+       // HttpPost postPwd = new HttpPost (app.getProperty("web.baseUrl")+"/login_password_page.php");
         List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("username"), username);
-        params.add(new BasicNameValuePair("password"), password);
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", password));
         params.add(new BasicNameValuePair("secure_session", "on"));
         params.add(new BasicNameValuePair("return", "index.php"));
         post.setEntity(new UrlEncodedFormEntity(params));
